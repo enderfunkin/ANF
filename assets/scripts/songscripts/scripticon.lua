70f00f92eb8d72dc9bf45323cf43793ce53a4841
NC <keyetpro@gmail.com>
2022-07-27 19:30:09 +0300

    some fixes

--- assets/scripts/songscripts/scripticon.lua
+++ assets/scripts/songscripts/scripticon.lua
@@ -1,50 +1,30 @@
-local gfSpeed = 1;
-
 function onBeatHit()
-
-	if (curBeat % gfSpeed == 0) then
-		if curBeat % (gfSpeed * 2) == 0 then
-			setProperty('iconP1.scale.x', 0.8 );
-			setProperty('iconP1.scale.y', 0.8 );
-			setProperty('iconP2.scale.x', 1.2 );
-			setProperty('iconP2.scale.y', 1.3 );
-
-			setProperty('iconP1.angle', -15);
-			setProperty('iconP2.angle', 15);
-		else
-			setProperty('iconP1.scale.x', 1.2 );
-			setProperty('iconP1.scale.y', 1.3 );
-			setProperty('iconP2.scale.x', 0.8 );
-			setProperty('iconP2.scale.y', 0.8 );
-
-			setProperty('iconP2.angle', -15);
-			setProperty('iconP1.angle', 15);
-		end
-
+	if curBeat % 2 == 0 then
+		setProperty('iconP1.angle', -15);
+		setProperty('iconP2.angle', 15);
+	else
+		setProperty('iconP1.angle', 15);
+		setProperty('iconP2.angle', -15);
 	end
-
+	doTweenAngle('iconP1Angle', 'iconP1', 0, 0.3, 'quadInOut');
+	doTweenAngle('iconP2Angle', 'iconP2', 0, 0.3, 'quadInOut');
 end
 
-function onUpdate()
+function onCountdownTick(counter)
+	if counter % 2 == 0 then
+		setProperty('iconP1.angle', -15);
+		setProperty('iconP2.angle', 15);
+	else
+		setProperty('iconP1.angle', 15);
+		setProperty('iconP2.angle', -15);
+	end
+	doTweenAngle('iconP1Angle', 'iconP1', 0, 0.3, 'quadInOut');
+	doTweenAngle('iconP2Angle', 'iconP2', 0, 0.3, 'quadInOut');
+end
 
-    if (getProperty('iconP1.angle') >= 0) then
-	    if ('iconP1.angle' ~= 0) then
-    	    setProperty('iconP1.angle', getProperty('iconP1.angle')-1);
-    	end
-    else
-        if ('iconP1.angle' ~= 0) then
-    	    setProperty('iconP1.angle', getProperty('iconP1.angle')+1);
-    	end
-    end
-
-    if (getProperty('iconP2.angle') >= 0) then
-	    if ('iconP2.angle' ~= 0) then
-    	    setProperty('iconP2.angle', getProperty('iconP2.angle')-1);
-    	end
-    else
-        if ('iconP2.angle' ~= 0) then
-    	    setProperty('iconP2.angle', getProperty('iconP2.angle')+1);
-    	end
-    end
-
+function onSongStart()
+	setProperty('iconP1.angle', -15);
+	setProperty('iconP2.angle', 15);
+	doTweenAngle('iconP1Angle', 'iconP1', 0, 0.3, 'quadInOut');
+	doTweenAngle('iconP2Angle', 'iconP2', 0, 0.3, 'quadInOut');
 end
